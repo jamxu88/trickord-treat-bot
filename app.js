8207c6eac9b0bf89aef1d9f0711080245e2fc3c3
James <james@jamesxu.dev>
2020-10-20 14:44:20 -0400

    Update app.js
    ooops

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ client.on('ready', function() {
     console.log(client.user.username);
 });
 client.on('message', (message) => {
+    function trick() {
+        message.channel.send("h!trick")
+    }
+    function treat() {
+        message.channel.send("h!treat")
+    }
     if (message.author.id == 755580145078632508) { 
         for(var i = 0; i < message.embeds.length; i++) {
             if(message.embeds[i].description.includes("h!trick")) {
-                setTimeout(message.channel.send("h!trick"),10)
+                setTimeout(trick,10)
                 console.log("Sniped.")
                 break;
             }else
             if (message.embeds[i].description.includes("h!treat")) {
-                setTimeout(message.channel.send("h!treat"),10)
+                setTimeout(treat,10)
                 console.log("Sniped.")
                 break;
             }
