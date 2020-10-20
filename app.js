2280479c327d1904c993415a277298c38b088a73
James <james@jamesxu.dev>
2020-10-20 13:51:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,19 @@ client.on('ready', function() {
     console.log(client.user.username);
 });
 client.on('message', (message) => {
-      if (message.author.id == 755580145078632508) { 
-          for(var i = 0; i < message.embeds.length; i++) {
-              if(message.embeds[i].description.includes("h!trick")) {
-                  message.channel.send("h!trick")
-                  console.log("Sniped.")
-                  break;
-              }else
-              if (message.embeds[i].description.includes("h!treat")) {
-                  message.channel.send("h!treat")
-                  console.log("Sniped.")
-                  break;
-              }
-          }
-      }
-  }
+    if (message.author.id == 755580145078632508) { 
+        for(var i = 0; i < message.embeds.length; i++) {
+            if(message.embeds[i].description.includes("h!trick")) {
+                message.channel.send("h!trick")
+                console.log("Sniped.")
+                break;
+            }else
+            if (message.embeds[i].description.includes("h!treat")) {
+                message.channel.send("h!treat")
+                console.log("Sniped.")
+                break;
+            }
+        }
+    }
 });
 client.login("your discord token");
